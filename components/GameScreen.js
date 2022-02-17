6e28723afd5c93883b25d6ddfc2a32947d7e7ddf
bhivam <shivamkajaria@gmail.com>
2022-02-17 04:37:34 -0500

    Adds Cat to box when clicked, but reloads every cat when updating

--- components/GameScreen.js
+++ components/GameScreen.js
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { useState } from 'react';
-import { StyleSheet, Button, View, Text } from 'react-native';
+import { StyleSheet, Button, View, Text, Image } from 'react-native';
 import { borderColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+
 
 const styles = StyleSheet.create({
     container: {
@@ -25,14 +26,18 @@ const styles = StyleSheet.create({
         borderColor: 'black',
         margin: 2,
         backgroundColor: 'grey',
-    }
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+    },
 })
 
 function GameScreen() {
     
     const initialBoxVals = [0, 1, 2, 3, 4, 5, 6, 7, 8].map((idNum) => { return {
         id: idNum,
-        checked: false,
+        checked: false, // eventually willl change to int of value 0 (initial state), 1 (checked by p1), 2 (checked by p3)
     }})
 
     const [Boxes, setBoxes] = useState(initialBoxVals)
@@ -41,32 +46,42 @@ function GameScreen() {
     <View style={styles.container}>
         <View style={styles.board}>
             <View style={styles.row}>
-                <Box id={1}/>
-                <Box id={1}/>
-                <Box id={1}/>
+                <Row start={0} end={3}/>
             </View>
             <View style={styles.row}>
-                <Box id={1}/>
-                <Box id={1}/>
-                <Box id={1}/>
+                <Row start={3} end={6}/>
             </View>
             <View style={styles.row}>
-                <Box id={1}/>
-                <Box id={1}/>
-                <Box id={1}/>
+                <Row start={6} end={9}/>
             </View>
         </View>
     </View>
   )
-
-    function Box(props) {
-
+    
+    function Row(props) {
         return (
-            <View style={styles.box} onTouchStart={() => {
-                setBoxes(Boxes.map((box) => {return box.id === props.id ? {...box, checked: true} : {...box}}))
-                console.log(Boxes)
-            }}/>
-    )}
+            Boxes.slice(props.start, props.end).map((box) => { 
+            if (box.checked === true) {    
+                    return (
+                    <View style={styles.box} onTouchStart={() => checkHandler(box.id)}>
+                        <Image style={styles.image} source={require("../assets/grumpycat.png")} />
+                    </View>)
+            } else {
+                return (
+                    <View style={styles.box} onTouchStart={() => checkHandler(box.id)}>
+                    </View>)
+            }
+            })
+        )
+    }
+  
+    function checkHandler(id) {
+        let boxes = [...Boxes]
+        console.log(boxes[id].checked)
+        boxes = boxes.map((box) => {return id === box.id ? {...box, checked: !box.checked} : box})
+        console.log(boxes[id].checked)
+        setBoxes(boxes)
+    }
 }
 
 export default GameScreen;
